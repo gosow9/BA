58c9f3f9d2330a0125f27c6fb7cb9ebb03b83601
Manuel <m-tischhauser@hotmail.com>
2020-02-28 15:49:39 +0100

    calibration hinzugefügt

---
+++ software/calibration/calibration.py
@@ -0,0 +1,82 @@
+import cv2
+import numpy as np
+import time
+
+def gstreamer_pipeline(
+    capture_width=1280,
+    capture_height=720,
+    display_width=1280,
+    display_height=720,
+    framerate=30,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc ! "
+        "video/x-raw(memory:NVMM), "
+        "width=(int)%d, height=(int)%d, "
+        "format=(string)NV12, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
+
+# To flip the image, modify the flip_method parameter (0 and 2 are the most common)
+print(gstreamer_pipeline(flip_method=0))
+cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
+if cap.isOpened():
+    window_handle = cv2.namedWindow("CSI Camera", cv2.WINDOW_AUTOSIZE)
+    # Window
+    while cv2.getWindowProperty("CSI Camera", 0) >= 0:
+        t_ref = time.time()
+
+        ret_val, img = cap.read()
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        # termination criteria
+        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+        # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+        objp = np.zeros((6 * 4, 3), np.float32)
+        objp[:, :2] = np.mgrid[0:4, 0:6].T.reshape(-1, 2)
+
+        # Arrays to store object points and image points from all the images.
+        objpoints = []  # 3d point in real world space
+        imgpoints = []  # 2d points in image plane.
+
+        # Find the chess board corners
+        ret, corners = cv2.findChessboardCorners(gray, (4, 6), None)
+
+        # If found, add object points, image points (after refining them)
+        if ret == True:
+            objpoints.append(objp)
+
+            corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+            imgpoints.append(corners2)
+
+            # Draw and display the corners
+            img = cv2.drawChessboardCorners(img, (4, 6), corners2, ret)
+
+
+            cv2.imshow("img", img)
+
+        #print fps
+        print(int(np.round(1/(time.time()-t_ref))))
+
+        # This also acts as
+        keyCode = cv2.waitKey(30) & 0xFF
+        # Stop the program on the ESC key
+        if keyCode == 27:
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+else:
+    print("Unable to open camera")
