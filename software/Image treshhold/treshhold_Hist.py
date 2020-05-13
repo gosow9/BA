49e5db5f52b79f035d599f6ed2be32b5c7475f4a
gosow9 <cedric.renda@hotmail.com>
2020-05-14 01:50:12 +0200

    TReshhold file backup

---
+++ software/Image treshhold/treshhold_Hist.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+import numpy as np
+import cv2 as cv
+import os
+import glob
+from matplotlib import pyplot as plt
+import matplotlib.animation as animation
+
+def opening_by_reconstruction(img, erosion_kernel, dilation_kernel):
+     im_eroded = cv.erode(img, erosion_kernel)
+     im_opened = reconstruction_by_dilation(im_eroded, img, dilation_kernel)
+     return im_opened
+
+
+def reconstruction_by_dilation(img, mask, dilation_kernel):
+     im_old = img.copy()
+     while(1):
+         img = cv.dilate(img, dilation_kernel)
+         img = cv.bitwise_and(img, mask)
+         if np.array_equal(im_old, img):
+             break
+         im_old = img.copy()
+     return img
+
+
+images = [cv.imread(file,0) for file in glob.glob("BacklightFeder/*.png")]
+imag = [cv.cvtColor(file, cv.COLOR_BGR2RGB) for file in images]
+
+#img = cv.imread('BacklightFeder/Full80.png',0)
+#imag = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+
+#img = images[0]
+#kernel = np.ones((3,3),np.uint8)
+#retval, treshhold = cv.threshold(img,114,255, cv.THRESH_BINARY)
+
+#for nr in range(90, 99, 5):
+plt.figure("original{}".format(50))
+plt.subplot(211)
+#retval, treshhold = cv.threshold(imag,114,255, cv.THRESH_BINARY)
+plt.imshow(imag[50])
+#plt.hist(imag.ravel(),256,[0,256])
+plt.subplot(212)
+plt.hist(imag[50].ravel(), bins=256, range=(0, 256), fc='k', ec='k')
+
+
+#fig2 = plt.figure("histos")
+# ims = []
+# for add in range(0,50,1):
+#     ims.append((plt.hist(imag[add].ravel(), bins=256, range=(0, 256), fc='k', ec='k')))
+
+# im_ani = animation.ArtistAnimation(fig2, ims, interval=50, repeat_delay=10,
+#                                     blit=True)
+# # To save this second animation with some metadata, use the following command:
+# im_ani.save('im.mp4')
+
+# plt.show()
+
+
+    #plt.figure("treshhold{}".format(nr))
+    #plt.imshow(treshhold)
+# im = cv.equalizeHist(im) 
+# blur = cv.GaussianBlur(img,(5,5),3)
+# th2 = cv.adaptiveThreshold(img,255,cv.ADAPTIVE_THRESH_MEAN_C,\
+#             cv.THRESH_BINARY,11,2)
+# ret3,th3 = cv.threshold(img,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
+
+#histogram equalisation
+# clahe = cv.createCLAHE(clipLimit=2.0, tileGridSize=(50,50))
+# cl1 = clahe.apply(img)
+# equ = cv.equalizeHist(img)
+
+#plotting everthing
+# plt.figure("otsu")
+# plt.imshow(th3)
+# plt.figure("mean")
+# plt.imshow(th2)
+# plt.figure("hist")
+# plt.hist(img.ravel(),256,[0,256]) 
+# plt.figure("histeq")
+# plt.imshow(cl1) 
+# plt.figure("histeqhist")
+# plt.hist(cl1.ravel(),256,[0,256]) 
+
+# plt.figure("eq")
+# plt.imshow(equ) 
+# plt.figure("eqhist")
+# plt.hist(equ.ravel(),256,[0,256]) 
+# ret3,th2 = cv.threshold(im,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
+# plt.hist(blur.ravel(),256,[0,256]); plt.show()
+# opening = cv.morphologyEx(th3, cv.MORPH_OPEN, kernel)
+# th1 = th3-th2
+
+# plt.figure("Blur")
+# plt.imshow(blur)
+# plt.figure("opening")
+# plt.imshow(opening)
+# plt.figure()
+# # plt.imshow(th2)
+# # plt.figure()
+
+
+
+
+
+# image = cv2.imread('test_schraube.png')
+
+# hist = cv2.calcHist([image],[0],None,[256],[0,256])
+# cv2.imshow('img', image)
+# #cv2.imshow('img', hist)
+# cv2.waitKey(0)
---
+++ software/Image treshhold/treshhold_times.py
@@ -0,0 +1,67 @@
+
+import cv2
+import time
+import numpy as np
+
+# gstreamer_pipeline returns a GStreamer pipeline for capturing from the CSI camera
+# Defaults to 1280x720 @ 60fps
+# Flip the image by setting the flip_method (most common values: 0 and 2)
+# display_width and display_height determine the size of the window on the screen
+
+
+def gstreamer_pipeline(
+    capture_width=3280,
+    capture_height=2464,
+    display_width=1280,
+    display_height=720,
+    framerate=20,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc ! "
+        "video/x-raw(memory:NVMM), "
+        "width=(int)%d, height=(int)%d, "
+        "format=(string)NV12, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
+
+hallo
+def show_camera():
+    # To flip the image, modify the flip_method parameter (0 and 2 are the most common)
+    print(gstreamer_pipeline(flip_method=0))
+    cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
+    if cap.isOpened():
+        window_handle = cv2.namedWindow("CSI Camera", cv2.WINDOW_AUTOSIZE)
+        # Window
+        while cv2.getWindowProperty("CSI Camera", 0) >= 0:
+            t = time.time()
+            ret_val, img = cap.read()
+            imggray  = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            (thresh, blackAndWhiteImage) = cv2.threshold(imggray, 127, 255, cv2.THRESH_BINARY)
+            t = t-time.time()
+            print(t)
+            #cv2.imshow("CSI Camera", img)
+            # This also acts as
+            keyCode = cv2.waitKey(30) & 0xFF
+            # Stop the program on the ESC key
+            if keyCode == 27:
+                break
+        cap.release()
+        cv2.destroyAllWindows()
+    else:
+        print("Unable to open camera")
+
+
+if __name__ == "__main__":
+    show_camera()
